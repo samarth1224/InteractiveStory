56615f10fca4813245e1f949e3300251db7d783d
Samarth2410 <74351365+samarth1224@users.noreply.github.com>
2026-01-14 00:41:22 +0530

    Update tools.py

--- Backend/agent/tools/tools.py
+++ Backend/agent/tools/tools.py
@@ -1,22 +1,25 @@
 from google.adk.tools.tool_context import ToolContext
-from google.adk.agents.callback_context import CallbackContext
-from agent.schemas.StoryPlanner import StoryPlotlinePlan
 from agent.schemas.NodeGenerator import StoryNodeGeneratorAgentResponse
 from typing import Dict
 
 
-
-def final_response_planner(context:ToolContext, final_response: StoryPlotlinePlan ):
-    print(type(final_response))
-    print(final_response)
-    return {'success': True}
-
 def save_states(context: ToolContext, states: Dict):
+    """
+      To save the states when agent wants to update them
+      Args:
+          states (dict) : states in a key-value pair to update or create them.
+      """
     for key,value in states.items():
         print(f'{key} : {value}')
         context.state[key] = value
     return {'success': True}
 def capture_final_response_node_generator(context: ToolContext, final_response: StoryNodeGeneratorAgentResponse):
+    """
+      Tool to capture the final response of the agent in a structured way
+      Args:
+          final_response (StoryNodeGeneratorAgentResponse): Final response of the agent
+                                              containing content,choices and image prompt
+      """
     print(type(final_response))
     print(final_response)
     return {'success':True}
